46548eca02e9834e844f68cf2262f7810e15d1ae
syedmwgilani <syedmwgilani@gmail.com>
2021-11-17 19:10:21 -0500

    Restructured App.js with header, nav and footer.

--- src/App.js
+++ src/App.js
@@ -4,15 +4,21 @@ import './App.css'
 export default function App() {
   return (
     <div className="App">
-      <div className="header">
+      <header>
         <h1>Habit Tracker</h1>
-        <nav>
-          <Link className="tab" to="/habit-tracker/habits">Habits</Link>
-          <Link className="tab" to="/habit-tracker/habit-template">Habit Template</Link>
-          <Link className="tab" to="/habit-tracker/habit-templates">Habit Templates</Link>
-        </nav>
-      </div>
+      </header>
+
+      <nav>
+        <Link className="tab" to="/habit-tracker/habits">Habits</Link>
+        <Link className="tab" to="/habit-tracker/habit-template">Habit Template</Link>
+        <Link className="tab" to="/habit-tracker/habit-templates">Habit Templates</Link>
+      </nav>
+
       <Outlet />
+
+      <footer>
+        <span>Explore more projects made by me <a href="https://github.com/syedmwgilani" title="github" target="_blank" rel="noreferrer">Github</a>.</span>
+      </footer>
     </div>
   );
 }
