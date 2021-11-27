ecf113589118be97dd858fb78cced45d490965d2
syedmwgilani <syedmwgilani@gmail.com>
2021-11-26 23:52:36 -0500

    Traffic direct added to index.js based on whether sessionStorage has been set in 404.html.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import {
   BrowserRouter,
   Routes,
   Route,
-  Link
+  Link,
+  Navigate
 } from "react-router-dom";
 import App from "./App";
 import Habits from './Habits';
@@ -13,10 +14,18 @@ import HabitTemplates from './HabitTemplates';
 import './index.css'
 
 const rootElement = document.getElementById("root");
+let direct = 'habits';
+if(sessionStorage.getItem('location')) {
+  direct = sessionStorage.getItem('location')
+  sessionStorage.removeItem('location')
+}
 render(
   <BrowserRouter>
     <Routes>
       <Route path="/habit-tracker/" element={<App />}>
+        {/* Traffic is being directed here, based on sessionStorage 'locations' value. 
+            Which is set in 404.html page.*/}
+        <Route path="/habit-tracker/" element={<Navigate to={'/habit-tracker/'+ direct} />}/>
         <Route path="habits" element={<Habits />} />
         <Route path="habit-template" element={<HabitTemplate />} />
         <Route path="habit-templates" element={<HabitTemplates />} />
@@ -25,7 +34,7 @@ render(
           element={
             <main style={{ padding: "1rem", textAlign: "center" }}>
               <p>Sorry you have navigated to a page that does not exist. 
-                Try going to the <Link to="/habit-tracker/habits">Habits page</Link> instead.</p>
+                Try going to the <Link to="/habit-tracker/">Habits page</Link> instead.</p>
             </main>
           }
         />
